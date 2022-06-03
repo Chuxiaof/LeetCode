f2612f236792b188926010b06bf8e445fb875b72
fengchuxiao <chuxiaof@uchicago.edu>
2022-06-03 14:56:13 -0500

    solution

--- LinkedList/Q2_AddTwoNumbers.py
+++ LinkedList/Q2_AddTwoNumbers.py
@@ -1,1 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummyHead = ListNode()
+        curr = dummyHead
+        carry = 0
+        while l1 or l2:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+            summ = carry+x+y
+            carry = summ // 10
+            curr.next = ListNode(summ % 10)
+            curr = curr.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
 
+        if (carry > 0):
+            curr.next = ListNode(carry)
+
+        return dummyHead.next
