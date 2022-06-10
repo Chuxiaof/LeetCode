e4375dab0a1fa7b5e1f41419b7296a57ee1f5396
fengchuxiao <chuxiaof@uchicago.edu>
2022-06-09 22:23:22 -0500

    add sol

--- README.md
+++ README.md
@@ -182,9 +182,11 @@
         - [x] Q94
         - [x] Q100
         - [x] Q101
+        - [x] Q102
         - [x] Q144
         - [x] Q145
         - [x] Q226
+        
 
 - ### Binary search trees: BSTs
     - [x] [Binary Search Tree Review (video)](https://www.youtube.com/watch?v=x6At0nzX92o&index=1&list=PLA5Lqm4uh9Bbq-E0ZnqTIa8LRaL77ica6)    
---
+++ Tree/Q572_SubtreeOfAnotherTree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        s = root
+        t = subRoot
+        if not s: 
+            return False
+        if self.isSameTree(s, t): 
+            return True
+        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p is q
+        
+        
+
+        
